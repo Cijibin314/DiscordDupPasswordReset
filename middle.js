56a29ba09a0bacaca5539a1a6e3a97b5631828cf
Cijibin314 <112033425+Cijibin314@users.noreply.github.com>
2024-08-03 15:10:39 -0400

    Update middle.js1

--- middle.js
+++ middle.js
@@ -2,6 +2,7 @@ let socket = new WebSocket("ws://localhost:8080");
 socket.onopen = function(e) {
   //alert("[open] Connection established");
   console.log("Connected")
+  socket.send(username)
 };
 
 socket.onmessage = function(event) {
